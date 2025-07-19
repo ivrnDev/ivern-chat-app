419fe6491bd3aaa55e791729cd05c17c56122ab2
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-07-19 11:55:03 +0800

    Setting up the application

---
+++ src/main/java/com/ivrndev/ivernchatapp/IvernChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.ivrndev.ivernchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvernChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvernChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivrndev/ivernchatapp/IvernChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivrndev.ivernchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvernChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
