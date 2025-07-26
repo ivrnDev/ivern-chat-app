0d36326c2dfa90c883ce43344df14bf2660301b6
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-07-26 10:31:25 +0800

    Initialize message controller

---
+++ src/main/java/com/ivrndev/ivernchatapp/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.ivrndev.ivernchatapp.controller;
+
+import com.ivrndev.ivernchatapp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Controller
+public class MessageController {
+
+}
