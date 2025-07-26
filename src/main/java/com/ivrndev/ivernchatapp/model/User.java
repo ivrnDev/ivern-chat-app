bba2799a20110de6fa1baada4939a6337818b510
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-07-26 11:25:06 +0800

    Add table and column names

--- src/main/java/com/ivrndev/ivernchatapp/model/User.java
+++ src/main/java/com/ivrndev/ivernchatapp/model/User.java
@@ -1,13 +1,32 @@
 package com.ivrndev.ivernchatapp.model;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
 }
